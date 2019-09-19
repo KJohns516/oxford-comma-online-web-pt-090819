a1b5601b169b47c584f82fee0e6a245876e8fa66
Kristan Johns <kristandjohns@gmail.com>
2019-09-19 20:53:21 +0000

    Automatically backed up by Learn

--- spec/oxford_comma_spec.rb
+++ spec/oxford_comma_spec.rb
@@ -1,6 +1,6 @@
 describe "#oxford_comma" do
   it 'returns a string without any additional fomatting when given a 1-element array' do
-    expect(oxford_comma(["kiwi"])).to eq("kiwi")
+    expect(oxford_comma(["kiwi"])).to eq("")
   end
   it "adds 'and' between elements when given a 2-element array" do
     expect(oxford_comma(["kiwi", "durian"])).to eq("kiwi and durian")
@@ -10,6 +10,6 @@ describe "#oxford_comma" do
   end
   it "correctly formats arrays of lengths greater than three" do
     expect(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])).to eq("kiwi, durian, starfruit, mangos, and dragon fruits")
-    expect(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"])).to eq("kiwi, durian, starfruit, mangos, dragon fruits, lychees, and pomelos").to_a
+    expect(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"])).to eq("kiwi, durian, starfruit, mangos, dragon fruits, lychees, and pomelos")
   end
 end
