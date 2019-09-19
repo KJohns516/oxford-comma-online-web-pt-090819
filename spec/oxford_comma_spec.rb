37029ef270b508cfb75541853b35c495d7e8d9a5
Kristan Johns <kristandjohns@gmail.com>
2019-09-19 21:09:19 +0000

    Automatically backed up by Learn

--- spec/oxford_comma_spec.rb
+++ spec/oxford_comma_spec.rb
@@ -6,10 +6,11 @@ describe "#oxford_comma" do
     expect(oxford_comma(["kiwi", "durian"])).to eq("kiwi and durian")
   end
   it "adds commas plus a final 'and' when given a 3-element array" do
+    .split
     expect(oxford_comma(["kiwi", "durian", "starfruit"])).to eq("kiwi, durian, and starfruit")
   end
   it "correctly formats arrays of lengths greater than three" do
-    expect(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])).to eq("kiwi, durian, starfruit, mangos, and dragon fruits").split
+    expect(oxford_comma(["kiwi", "durian", "starfruit", "mangos","dragon fruits"])).to eq("kiwi, durian, starfruit, mangos, and dragon fruits")
     expect(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"])).to eq("kiwi, durian, starfruit, mangos, dragon fruits, lychees, and pomelos")
   end
 end
